2ae58288ffaff4d973fac9fbcd8205591661657c
mosng <2013934584@qq.com>
2019-03-08 09:49:09 +0800

    first commit

---
+++ moyui_mosng_top/application/api/controller/Banner.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mosng
+ * Date: 2019/3/2
+ * Time: 10:27
+ */
+
+namespace app\api\controller;
+
+
+class Banner
+{
+    public function getBanner()
+    {
+
+    }
+}
